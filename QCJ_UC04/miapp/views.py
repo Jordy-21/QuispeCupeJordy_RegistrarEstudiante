0f2e2ff8d776ca9dd08679489cabb173d56fa5a5
Jordy Quispe <cupejordy@gmail.com>
2022-07-26 23:12:49 -0500

    Parte 3 - Registrar Estudiantes - OK

---
+++ QCJ_UC04/miapp/views.py
@@ -0,0 +1,109 @@
+from tkinter import E
+from django.shortcuts import render, HttpResponse, redirect
+from miapp.models import Estudiante
+
+# Create your views here.
+def index(request):
+    estudiantes = ['Quispe Cupe Jordy',
+                   'Jordy Quispe Cupe',
+                   'Quispe Cupe Jordy',
+                   'Jordy Quispe Cupe']
+ 
+    return render(request,'index.html', {
+        'titulo':'Inicio',
+        'mensaje':'Proyecto Web - UC4 LP3 25-07-2022',
+        'estudiantes': estudiantes
+    })
+
+def saludo(request):
+    return render(request,'saludo.html',{
+         'titulo': 'Saludo',
+         'autor_saludo':'Ing. Quispe Cupe Jordy Eusebio'
+    })
+       
+def crear_estudiante(request, codigo, dni, nombre, apepat, apemat, direccion, estado):
+    estudiante = Estudiante(
+        codigo = codigo,
+        dni = dni,
+        nombre = nombre,
+        apepat = apepat,
+        apemat = apemat,
+        direccion = direccion,
+        estado =   estado
+    )
+    estudiante.save()
+    return HttpResponse(f"Estudiante Creado: {estudiante.codigo} - {estudiante.dni} - {estudiante.nombre} - {estudiante.apepat} - {estudiante.apemat} - {estudiante.direccion} - {estudiante.estado}")
+
+def buscar_estudiante(request):
+    try:
+        estudiante = Estudiante.objects.get(id=1000)
+        resultado = f"""Articulo: 
+                        <br> <strong>ID:</strong> {estudiante.id} 
+                        <br> <strong>Codigo:</strong> {estudiante.codigo} 
+                        <br> <strong>DNI:</strong> {estudiante.dni}
+                        <br> <strong>Nombre:</strong> {estudiante.nombre} 
+                        <br> <strong>Apellido Paterno:</strong> {estudiante.apepat}
+                        <br> <strong>Apellido Materno:</strong> {estudiante.apemat} 
+                        <br> <strong>Dirección:</strong> {estudiante.direccion}
+                        <br> <strong>Estado:</strong> {estudiante.estado}
+                        """
+    except:
+        resultado = "<h1> Estudiante No Encontrado, intentalo otra vez </h1>"
+    return HttpResponse(resultado)
+
+def editar_estudiante(request, id):
+    estudiante = Estudiante.objects.get(pk=id)
+ 
+    estudiante.codigo = 2017110656
+    estudiante.dni = 75965737
+    estudiante.nombre = "JORDY EUSEBIO"
+    estudiante.apepat = "Quispe"
+    estudiante.apemat = "Cupe"
+    estudiante.direccion = "Villa el Salvador"
+    estudiante.estado = False
+ 
+    estudiante.save()
+    return HttpResponse(f"Estudiante Editado: {estudiante.codigo} - {estudiante.dni} - {estudiante.nombre} - {estudiante.apepat} - {estudiante.apemat} - {estudiante.direccion} - {estudiante.estado}")
+
+def listar_estudiantes(request):
+    estudiantes = Estudiante.objects.all();#estudiante
+    """estudiantes = Estudiante.objects.filter(
+            Q(nombre__contains="Py") |
+            Q(nombre__contains="Hab")
+    )"""
+    return render(request, 'listar_estudiantes.html',{
+        'estudiantes': estudiantes,
+        'nombre': 'Listado de estudiantes'
+    })
+
+def eliminar_estudiante(request, id):
+    estudiante = Estudiante.objects.get(pk=id)
+    estudiante.delete()
+    return redirect('listar_estudiantes')
+
+def save_estudiante(request):
+    if request.method == 'GET':
+        codigo = request.GET['codigo']
+        dni = request.GET['dni']
+        nombre = request.GET['nombre']
+        apepat = request.GET['apepat']
+        apemat = request.GET['apemat']
+        direccion = request.GET['direccion']
+        estado = request.GET['estado']
+ 
+        estudiante = Estudiante(
+            codigo = codigo,
+            dni = dni,
+            nombre = nombre,
+            apepat = apepat,
+            apemat = apemat,
+            direccion = direccion,
+            estado =   estado
+        )
+        estudiante.save()
+        return HttpResponse(f"Estudiante Creado: {estudiante.codigo} - {estudiante.dni} - {estudiante.nombre} - {estudiante.apepat} - {estudiante.apemat} - {estudiante.direccion} - {estudiante.estado}")
+    else:
+        return HttpResponse("<h2>No se ha podido registrar el estudiante</h2>")
+ 
+def create_estudiante(request):
+    return render(request, 'create_estudiante.html')
